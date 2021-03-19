56303232f0b16e07d00c5b4d48b04e0506a40009
rafdy-rayan <80346935+rafdy-rayan@users.noreply.github.com>
2021-03-19 12:24:31 +0100

    before didn't pass the tests, now passes all tests

--- assignment1/src/main/java/books/BookShop.java
+++ assignment1/src/main/java/books/BookShop.java
@@ -1,4 +1,4 @@
-package books;
+package main.java.books;
 
 public class BookShop {
 
@@ -18,7 +18,45 @@ public class BookShop {
      * @return the cost in euro with the discount
      */
     public double cost(int[] books){
-        //TODO complete
-        return 0.0;
+        int reduced = 0;
+        int not_reduced = 0;
+        double prize = 0;
+        double discount;
+        for (int i=0; i < books.length; i++ ) {
+        	if (books[i] > 0) {
+        		reduced += 1;
+        		if (books[i]>1)
+        			not_reduced += books[i] - 1;
+        	}
+        }
+       
+        switch(reduced) {
+        	case 2: 
+        		discount =  0.07;
+        		break;
+        	case 3: 
+        		discount = 0.14;
+        		break;
+        	case 4: 
+        		discount = 0.28;
+        		break;
+        	case 5: 
+        		discount = 0.35;
+        		break;
+        	default:
+        		discount = 0;
+        		break;
+        	
+        }
+        prize = (not_reduced*8) + (reduced*8) - (discount*reduced*8);
+        
+        return (double) Math.round(prize * 100d) / 100d;
     }
+    
+    
+   
 }
+
+
+
+
